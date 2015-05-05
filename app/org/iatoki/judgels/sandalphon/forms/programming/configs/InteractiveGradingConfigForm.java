1a5abde94c1afa510262f10b9817418aebd01f77
Ashar Fuadi <fushar@gmail.com>
2015-05-06 03:59:55 +0700

    Add interactive grading engine
    Part of ia-toki/judgels#141

---
+++ app/org/iatoki/judgels/sandalphon/forms/programming/configs/InteractiveGradingConfigForm.java
@@ -0,0 +1,5 @@
+package org.iatoki.judgels.sandalphon.forms.programming.configs;
+
+public final class InteractiveGradingConfigForm extends SingleSourceFileWithoutSubtasksBlackBoxGradingConfigForm {
+    public String communicator;
+}
