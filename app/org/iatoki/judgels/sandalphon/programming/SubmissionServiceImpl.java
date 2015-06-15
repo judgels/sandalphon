2e7fd94ca5d93f5348f59d74ce89681f6313f583
Ashar Fuadi <fushar@gmail.com>
2015-06-15 18:11:56 +0700

    Adapt Sealtiel package restructure
    Part of ia-toki/judgels#203

--- app/org/iatoki/judgels/sandalphon/programming/SubmissionServiceImpl.java
+++ app/org/iatoki/judgels/sandalphon/programming/SubmissionServiceImpl.java
@@ -5,7 +5,7 @@ import org.iatoki.judgels.sandalphon.models.daos.interfaces.programming.GradingD
 import org.iatoki.judgels.sandalphon.models.daos.interfaces.programming.ProgrammingSubmissionDao;
 import org.iatoki.judgels.sandalphon.models.domains.programming.GradingModel;
 import org.iatoki.judgels.sandalphon.models.domains.programming.ProgrammingSubmissionModel;
-import org.iatoki.judgels.sealtiel.client.Sealtiel;
+import org.iatoki.judgels.sealtiel.Sealtiel;
 
 public final class SubmissionServiceImpl extends AbstractSubmissionServiceImpl<ProgrammingSubmissionModel, GradingModel> {
     public SubmissionServiceImpl(ProgrammingSubmissionDao submissionDao, GradingDao gradingDao, Sealtiel sealtiel, String gabrielClientJid) {
