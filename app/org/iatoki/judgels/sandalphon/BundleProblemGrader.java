c8ebefdeac27aa1c4e35b35a6e457b52bdaea6b5
Jordan Fernando <fernandojordan.92@gmail.com>
2015-06-29 14:55:24 +0700

    Refactor package for BundleProblemGrader

--- app/org/iatoki/judgels/sandalphon/BundleProblemGrader.java
+++
@@ -1,7 +0,0 @@
-package org.iatoki.judgels.sandalphon;
-
-import java.io.IOException;
-
-public interface BundleProblemGrader {
-    BundleGradingResult gradeBundleProblem(String problemJid, BundleAnswer bundleAnswer) throws IOException;
-}
